26d14daa7bf80fb4f51b685af7d8abcd9636d585
master.dark.dance@gmail.com <master.dark.dance@gmail.com>
2025-07-26 18:47:02 +0200

    wip

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Regen",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/en/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "to do Specjalistyczna regeneracja akumulatorów do respiratora medycznego Ventilator Puritan Bennett 980 REF10086042 Medtronic / Covidien\nSprzedaż baterii Sprzedaż baterii Oferujemy sprzedaż baterii do respiratorów Covidien Puritan Bennett 980.\n-Jeśli Chcesz kupić baterię - skontaktuj się z nami\nRegenerację baterii Oferujemy regenerację starych baterii do respiratorów Covidien Puritan Bennett 980.\n​\rOpis\rParametry\rdo pobrania\rGwarancia\rCena\rDostęmpnosc\rThe style parameter is set to a color style.\nThis will set the background to a lighter version of the chosen style color as configured in your theme variant.\nOnly the color parameter is set.\nThis will set the background to a lighter version of the chosen CSS color value.\nThe default style will set the background to your --MAIN-BG-color as configured for your theme variant resembling the default style but with different color.\nThe style parameter is set to a severity style.\nThis will set the background to a lighter version of the chosen style color as configured in your theme variant and also affects the chosen icon.\nThe style parameter affects how the color parameter is applied.\nThis will set the background to a lighter version of the chosen CSS color value and also affects the chosen icon.\nThe style parameter affects how the color parameter is applied.\nThis will set the background to a lighter version of the chosen CSS color value and also affects the chosen icon.\nThe style parameter affects how the color parameter is applied.\nThis will set the background to a lighter version of the chosen CSS color value and also affects the chosen icon.\nKupic Заголовок 1 Заголовок 2 Заголовок 3 Ячейка 1-1 Ячейка 1-2 Ячейка 1-3 Ячейка 2-1 Ячейка 2-2 Ячейка 2-3 Ячейка 3-1 Ячейка 3-2 Ячейка 3-3 Callouts can have custom titles\rLike this one.\nTitle-only callout\rAre callouts foldable?\rYes! In a foldable callout, the contents are hidden when the callout is collapsed\nAre callouts foldable?\rYes! In a foldable callout, the contents are hidden when the callout is collapsed\nCaution\rAdvises about risks or negative outcomes of certain actions.\nAdvises about risks or negative outcomes of certain actions.\nAdvises about risks or negative outcomes of certain actions.\nDetails\rAdvises about risks or negative outcomes of certain actions.\nTitle-only callout\rAdvises about risks or negative outcomes of certain actions.\nDetails\rJust a box\nstateDiagram-v2 open: Open Door closed: Closed Door locked: Locked Door open --\u003e closed: Close closed --\u003e locked: Lock locked --\u003e closed: Unlock closed --\u003e open: Open %%{init:{\"theme\":\"forest\"}}%% erDiagram CUSTOMER }|..|{ DELIVERY-ADDRESS : has CUSTOMER ||--o{ ORDER : places CUSTOMER ||--o{ INVOICE : \"liable for\" DELIVERY-ADDRESS ||--o{ ORDER : receives INVOICE ||--|{ ORDER : covers ORDER ||--|{ ORDER-ITEM : includes PRODUCT-CATEGORY ||--|{ PRODUCT : contains PRODUCT ||--o{ ORDER-ITEM : \"ordered in\"",
+    "description": "to do Specjalistyczna regeneracja akumulatorów do respiratora medycznego Ventilator Puritan Bennett 980 REF10086042 Medtronic / Covidien\nSprzedaż baterii Sprzedaż baterii Oferujemy sprzedaż baterii do respiratorów Covidien Puritan Bennett 980.\n-Jeśli Chcesz kupić baterię - skontaktuj się z nami\nRegenerację baterii Oferujemy regenerację starych baterii do respiratorów Covidien Puritan Bennett 980.\n​\rOpis\rParametry\rdo pobrania\rGwarancia\rCena\rDostęmpnosc\rThe style parameter is set to a color style.",
+    "tags": [],
+    "title": "Regen",
+    "uri": "/en/index.html"
+  },
+  {
+    "breadcrumb": "Regen",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/en/tags/index.html"
+  }
+]
---
+++ searchindex.pl.js
@@ -0,0 +1,106 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do respiratora medycznego Ventilator Puritan Bennett 980 REF10086042 Medtronic / Covidien\nSprzedaż baterii Oferujemy sprzedaż baterii do respiratorów Covidien Puritan Bennett 980.\nJeśli Chcesz kupić baterię - skontaktuj się z nami\nKupic\nRegenerację baterii na zamowienie Oferujemy regenerację starych baterii do respiratorów Covidien Puritan Bennett 980 na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym Covidien Puritan Bennett 980.\nZamówić ​\rParametry\rDokumentacja\rGwarancia\rCena\rDostęmpnosc\rNapięcie 25,9v Pojemność 112 Wh Chemia Litowo-jonowa Waga 1,5 kg Wymiary 242 x70 x 35mm Stan ogniw nowy Segmentow 7 Ogniw 21 -Puritan Bennett 980 technical specifications en.pdf\n-Puritan Bennett 980 technical specifications pl.pdf\n-Puritan Bennett 980 operator manual 1.pdf\n-Puritan Bennett 980 operator manual 2.pdf\n-Puritan Bennett 980 Capnography.pdf\n-Puritan Bennett 980 Battery pack handling.pdf\nZ zastrzeżeniem użytkowania. (Nie jest przeznaczony do przechowywania w magazynie)\nDarmowa wymiana lub naprawa 6 miesiency Żywotność bateri 24-48 miesiency Żywotność bateri 24 miesiency 2000 pln Żywotność bateri 36 miesiency 2500 pln Żywotność bateri 48 miesiency 3000 pln Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "Bennett 980",
+    "tags": [],
+    "title": "Acumulator dla Bennett 980",
+    "uri": "/accum/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Ważne\rRegenerujemy akumulatory do sprzętu medycznego od ponad 2 lat.\nWspółpracujemy ze szpitalami w całej Polsce\nRegeneracja Akumulatorw dla Puritan Bennett 980 Specjalistyczna regeneracja akumulatorów do respiratora medycznego Puritan Bennett 980\n(kod producenta: REF 10086042, Medtronic / Covidien). Usługa dostępna na terenie całej Polski i UE. Jak działa nasz serwis? Klient wysyła do nas swój stary zużyty akumulator (oryginał REF 10086042) ktury nie przyhodze testy.\rPrzeprowadzamy pełną WYMIANA OGNIW NA NOWE litowo-jonowych – naprawa baterii respiratora PB980.\rKażda bateria przechodzi testy:\r- pojemności i napięcia\r- temperatury\r- zabezpieczenia termiczne i zwarciowe\r- testy pracy pod obciążeniem\rJeśli akumulator spełnia wszystkie normy – odsyłamy go do klienta, z gwarancją sprawności.\rDoświadczenie i niezawodność 🔁 Regenerujemy baterie do PB980 od kilku lat na terenie Polski.\r🔋 Nasze akumulatory są w pełni kompatybilne ze starszym oraz nowym oprogramowaniem respiratorów PB980.\r🔐 Wysoka jakość, stabilność pracy i niezawodność – jak nowe oryginalne baterie OEM.\rDlaczego warto? 💰 Tańsza alternatywa dla oryginalnych akumulatorów, których brak na rynku.\r📜 Otrzymujesz: raport testowy, gwarancję działania i dokument sprzedaży.\r🚚 Wysyłka kurierska w Polsce i Unii Europejskiej.\r🧰 Możliwość współpracy z serwisami medycznymi i technikami biomedycznymi.\rSprzedarz za granice Działamy na terenie całej Polski i UE Możliwość stałej współpracy. Wystawiamy faktury bez VAT (działalność zwolniona z podatku VAT)",
+    "description": "Ważne\rRegenerujemy akumulatory do sprzętu medycznego od ponad 2 lat.\nWspółpracujemy ze szpitalami w całej Polsce\nRegeneracja Akumulatorw dla Puritan Bennett 980 Specjalistyczna regeneracja akumulatorów do respiratora medycznego Puritan Bennett 980\n(kod producenta: REF 10086042, Medtronic / Covidien). Usługa dostępna na terenie całej Polski i UE. Jak działa nasz serwis? Klient wysyła do nas swój stary zużyty akumulator (oryginał REF 10086042) ktury nie przyhodze testy.\rPrzeprowadzamy pełną WYMIANA OGNIW NA NOWE litowo-jonowych – naprawa baterii respiratora PB980.\rKażda bateria przechodzi testy:\r- pojemności i napięcia\r- temperatury\r- zabezpieczenia termiczne i zwarciowe\r- testy pracy pod obciążeniem\rJeśli akumulator spełnia wszystkie normy – odsyłamy go do klienta, z gwarancją sprawności.\rDoświadczenie i niezawodność 🔁 Regenerujemy baterie do PB980 od kilku lat na terenie Polski.\r🔋 Nasze akumulatory są w pełni kompatybilne ze starszym oraz nowym oprogramowaniem respiratorów PB980.\r🔐 Wysoka jakość, stabilność pracy i niezawodność – jak nowe oryginalne baterie OEM.\rDlaczego warto? 💰 Tańsza alternatywa dla oryginalnych akumulatorów, których brak na rynku.\r📜 Otrzymujesz: raport testowy, gwarancję działania i dokument sprzedaży.\r🚚 Wysyłka kurierska w Polsce i Unii Europejskiej.\r🧰 Możliwość współpracy z serwisami medycznymi i technikami biomedycznymi.\rSprzedarz za granice Działamy na terenie całej Polski i UE Możliwość stałej współpracy. Wystawiamy faktury bez VAT (działalność zwolniona z podatku VAT)",
+    "tags": [],
+    "title": "Regenerujemo akumulatory",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do GE Monitor B450 02379 U80296-1R01 REFU80296-1R01\nSprzedaż baterii Kupic Regenerację baterii na zamowienie Oferujemy regenerację starych baterii na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym\nZamówić ​\rParametry\rDostęmpnosc\rNapięcie 10.8V Pojemność 3800mAh/41.04Wh Chemia Litowo-jonowa Waga 0,7 kg Stan ogniw nowy Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "FLEX-3S2P",
+    "tags": [],
+    "title": "Acumulator FLEX-3S2P",
+    "uri": "/flex-3s2p/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do Respirator Newport Medical HT-70 Plus REFBAT3271A\nSprzedaż baterii Kupic Regenerację baterii na zamowienie Oferujemy regenerację starych baterii na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym\nZamówić ​\rParametry\rDostęmpnosc\rNapięcie 14,4 V Pojemność 85 Wh Chemia Litowo-jonowa Waga 0,7 kg Stan ogniw nowy Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "BAT3271A",
+    "tags": [],
+    "title": "Acumulator BAT3271A",
+    "uri": "/bat3271a/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do Philips IntelliVue MP2 / M4607A REFPhilips M4607A\nSprzedaż baterii Kupic Regenerację baterii na zamowienie Oferujemy regenerację starych baterii na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym\nZamówić ​\rParametry\rDostęmpnosc\rNapięcie 10.8V Pojemność 1500mAh 16.20Wh Chemia Litowo-jonowa Waga 0,6 kg Stan ogniw nowy Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "M4607A",
+    "tags": [],
+    "title": "Acumulator M4607A",
+    "uri": "/m4607a/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do GE Healthcare ECG Monitor B650 B105 B125 Batteries 122118-35511 REFU80206AR01\nSprzedaż baterii Kupic Regenerację baterii na zamowienie Oferujemy regenerację starych baterii na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym\nZamówić ​\rParametry\rDostęmpnosc\rNapięcie 10.8V Pojemność 5.1Ah/55.1WH Chemia Litowo-jonowa Waga 0,9 kg Stan ogniw nowy Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "U80206AR01",
+    "tags": [],
+    "title": "Acumulator U80206AR01",
+    "uri": "/u80206ar01/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Specjalistyczna regeneracja akumulatorów do Akumulator dla respiratora Hamilton Medical C1, T1 REF369108\nSprzedaż baterii Kupic Regenerację baterii na zamowienie Oferujemy regenerację starych baterii na zamówienie.\nKlient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości, przeprogramować kontroler, Wykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię. Jeśli okaże się, że elektronika w wysłanym przez Ciebie akumulatorze jest nieodwracalnie uszkodzona i nie można jej naprawić, odeślemy Ci oryginalny akumulator. Koszt dostawy w obu kierunkach ponosi Klient za pośrednictwem właściwego pośrednika transportowego (DHL, InPost, UPS0 …). Te baterie będą działać zarówno ze starym, jak i nowym oprogramowaniem układowym\nZamówić ​\rParametry\rDostęmpnosc\rNapięcie 10.8V Pojemność 6,66Ah 72Wh Chemia Litowo-jonowa Waga 0,6 kg Stan ogniw nowy Sprzedaz Regeneracja na zamowienie Zapytaj o dostępność robimy za 2-4 dni",
+    "description": "REF 369108",
+    "tags": [],
+    "title": "Acumulator REF 369108",
+    "uri": "/ref369108/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Odpowiedzi na standardowe pytania\nOperation Squid Spaghetti Czy można po prostu kupić baterie, nie wysyłając starych?\rTak, możesz, ale sprawdź proszę, czy jest to możliwe poprzez formularz kontaktowy\nWystawiasz fakturę z VAT\rNie, wystawiamy fakturę bez VAT\nDostawa na terenie Polski za pośrednictwem jakich pośredników?\rKażdy, który będzie dla Ciebie wygodny.\nCzy dostarczacie na terenie Europy?\rTak, wysyłamy, ale należy ustalić koszt wysyłki.\nOstrożność\rGwarancja obowiązuje pod warunkiem stosowania baterii.\nZgodnie ze specyfikacją akumulatory nie są przeznaczone do bardzo długiego przechowywania bez możliwości ładowania.",
+    "description": "questions",
+    "tags": [],
+    "title": "Pytania",
+    "uri": "/questions/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Regenerację baterii Klient przysłał nam starą baterię, która nie przeszła testów respiratora.\nwymieniamy jednostki wewnętrzne na nowe certyfikowane najwyższej jakości,\rprzeprogramować kontroler,\rWykonamy testy (ładowanie/rozładowanie, temperatura, zwarcie, rezystancja wewnętrzna) jeśli wszystkie testy wypadną pomyślnie, wystawiamy fakturę i po zapłaceniu wysyłamy Ci baterię.\rJeśli chcesz zamówić regenerację akumulatora, schemat znajdziesz poniżej.\nSchemat stateDiagram-v2\rKlient: Klient\rSerwis: Serwis\rBadanie: Badanie\rWymiana: Wymiana ogniw\rTesty: Testy\rProgramowanie: Programowanie\rKlient --\u003e Serwis: stara bateria\rSerwis --\u003e Badanie: stara bateria\rBadanie --\u003e Wymiana: stara bateria\rWymiana --\u003e Programowanie: regenerowana bateria\rProgramowanie --\u003e Testy: regenerowana bateria\rTesty --\u003e Serwis: regenerowana bateria\rSerwis --\u003e Klient: regenerowana bateria\rstyle Klient fill:mediumpurple,stroke:rebeccapurple,stroke-width:4px\rstyle Serwis fill:mediumvioletred,stroke:purple,stroke-width:4px\rstyle Badanie,Wymiana,Programowanie,Testy fill:dodgerblue,stroke:royalblue,stroke-width:4px",
+    "description": "how to work",
+    "tags": [],
+    "title": "Jak to działa",
+    "uri": "/how_to_work/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Magic in new window\nOrlen https://www.orlenpaczka.pl/\nInpost https://inpost.pl/znajdz-paczkomat\nDPD https://nadaj.dpd.com.pl/\nFedEx https://www.fedex.com/pl-pl/home.html\nPocztex https://www.pocztex.pl/\nDHL https://dhl24.com.pl\nUPS https://www.ups.com/ship/basic/smallbusiness?tx=17759348707929668\u0026loc=pl_PL\nGLS https://gls-group.com/PL/pl/sledzenie-paczek/\nDostawa po europe https://www.euro-paka.pl",
+    "description": "Dostawa",
+    "tags": [],
+    "title": "Dostawa",
+    "uri": "/delivery/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "Loading…",
+    "description": "Contact the seller",
+    "tags": [],
+    "title": "Kontakt",
+    "uri": "/contact/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Kategorie",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Regenerujemo akumulatory",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tagi",
+    "uri": "/tags/index.html"
+  }
+]
